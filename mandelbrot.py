d2152d5df3c74fde924dcac88742678c12e37554
Christian Bertram <cbertramdk@gmail.com>
2016-02-14 17:42:41 +0000

    Initial mandelbrot.py
    A recursive mandelbrot function is used to find out how many iterations it takes to prove the complex number is not part of the Mandelbrot set, or if it is. A black pixel is printed to the png if the number is approximated to be part of the Mandelbrot set, and a color corresponding to how close if not part of the set.
    Variables are at the top of the program, are used to specify which part of the set, and with how much precision, it is going to be approximated.

---
+++ mandelbrot.py
@@ -0,0 +1,50 @@
+import png
+import math, cmath
+import sys
+
+sys.setrecursionlimit(1000) # the recursion limit needs to be increased if you make MAXIT higher
+
+WIDTH, HEIGHT = 1000, 1000
+MAXIT = 500 # Needs to be increased when zooming in, to get detail
+X, XL =  -.5, 2
+Y, YL = 0,  2
+
+#Coordinates as center (normally they are top-left corner)
+X -= XL/2
+Y += YL/2
+
+def mandelbrot(z, c, it):
+    if math.sqrt(z.real**2 + z.imag**2) > 2:
+        return it
+    elif it < MAXIT:
+        return mandelbrot(z**2+c, c, it+1)
+    else:
+        return None
+
+def main():
+    image = []
+    for h in range(HEIGHT):
+        print h
+        row = ()
+        for w in range(WIDTH):
+            x = X + w * XL/WIDTH
+            y = Y - h * YL/HEIGHT
+            m = mandelbrot(0, complex(x, y), 0)
+
+            if m == None:
+                row += (0,)
+            else:
+                m = m # could be changed to m = 2*m or something like that, when zoomin in, to get more detail 
+                #Greyscale
+                row += (int((255.0/MAXIT*m)%255),)
+                #Color
+                #row = row + (math.floor(255/MAXIT*m), math.floor(255/MAXIT*m/5), 0,) #Change the equation to make fancy colors
+        image.append(row)
+
+    f = open("mandelbrot2_test.png", "wb")
+    w = png.Writer(WIDTH, HEIGHT, greyscale=True)
+    w.write(f, image)
+    f.close()
+
+if __name__ == "__main__":
+    main()
